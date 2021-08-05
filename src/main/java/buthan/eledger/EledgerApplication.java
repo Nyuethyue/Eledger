93e6be01b0248661184cbef18a70ab376381ed19
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-08-05 15:31:58 +0400

    BITS-1817: Create ELEDGER application skeleton
    Change-Id: I2eddfc2ff80181df4292adb86b31b714a91b6dab

---
+++ src/main/java/buthan/eledger/EledgerApplication.java
@@ -0,0 +1,13 @@
+package buthan.eledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buthan/eledger/EledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package buthan.eledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
