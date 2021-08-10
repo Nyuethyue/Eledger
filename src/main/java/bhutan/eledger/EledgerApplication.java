cbe0cbd4da0d2a43cbd8d3bf023c2db542940fae
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-08-10 14:15:07 +0400

    BITS-1817: package name fix
    Change-Id: Iafe1d4894d71e43db9f3903aaf29fe89c2e20867

--- src/main/java/bhutan/eledger/EledgerApplication.java
+++ src/main/java/bhutan/eledger/EledgerApplication.java
@@ -1,4 +1,4 @@
-package buthan.eledger;
+package bhutan.eledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/bhutan/eledger/EledgerApplicationTests.java
+++ src/test/java/bhutan/eledger/EledgerApplicationTests.java
@@ -1,4 +1,4 @@
-package buthan.eledger;
+package bhutan.eledger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
