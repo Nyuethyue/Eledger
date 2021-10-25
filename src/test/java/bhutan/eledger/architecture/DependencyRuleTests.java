90d0139deba7a0cefe0f2314a65b8245f8541f81
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-10-25 17:03:33 +0400

    Test for Architecture check
    Change-Id: I7453edf2a30208c6cbc436842620226b86335cbb

---
+++ src/test/java/bhutan/eledger/architecture/DependencyRuleTests.java
@@ -0,0 +1,71 @@
+package bhutan.eledger.architecture;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class DependencyRuleTests {
+
+    @Test
+    void validateRegistrationContextArchitecture() {
+        HexagonalArchitecture.boundedContext("bhutan.eledger")
+
+                .withDomainLayer("domain")
+
+                .withAdaptersLayer("adapter")
+                .incoming("web")
+                .outgoing("persistence")
+                .and()
+
+                .withApplicationLayer("application")
+                .services("service")
+                .incomingPorts("port.in")
+                .outgoingPorts("port.out")
+                .and()
+
+                .withConfiguration("configuration")
+                .check(new ClassFileImporter()
+                        .importPackages("bhutan.eledger.."));
+    }
+
+    @Test
+    void testPackageDependencies() {
+        noClasses()
+                .that()
+                .resideInAPackage("bhutan.eledger.domain..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("bhutan.eledger.application..")
+                .check(new ClassFileImporter()
+                        .importPackages("bhutan.eledger.."));
+
+        noClasses()
+                .that()
+                .resideInAPackage("bhutan.eledger.application..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("bhutan.eledger.adapter..")
+                .check(new ClassFileImporter()
+                        .importPackages("bhutan.eledger.."));
+
+        noClasses()
+                .that()
+                .resideInAPackage("bhutan.eledger.application..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("bhutan.eledger.adapter..")
+                .check(new ClassFileImporter()
+                        .importPackages("bhutan.eledger.."));
+
+        noClasses()
+                .that()
+                .resideInAPackage("bhutan.eledger.adapter.persistence..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("bhutan.eledger.web..")
+                .check(new ClassFileImporter()
+                        .importPackages("bhutan.eledger.."));
+    }
+
+}
