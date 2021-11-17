2acde8b24c7ea16bf455cbc3576651a9b16c1c88
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-17 12:32:59 +0400

    BP-308_ELEDGER_MOVE_REFACTORING

--- src/main/java/bhutan/eledger/adapter/web/ref/paymentmode/PaymentModeController.java
+++ src/main/java/bhutan/eledger/adapter/web/ref/paymentmode/PaymentModeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/payment/modes")
+@RequestMapping("/payment/mode")
 @RequiredArgsConstructor
 class PaymentModeController {
 
