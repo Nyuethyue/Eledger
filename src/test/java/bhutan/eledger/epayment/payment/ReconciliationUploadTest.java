c4f8fe158ba0953e34240a605a375e409da16ac2
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-19 16:41:59 +0400

    BP-359 Bank excel statemenst upload

--- src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
+++ src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
@@ -1,17 +1,30 @@
 package bhutan.eledger.epayment.payment;
 
-import bhutan.eledger.common.excel.ReconciliationExcelLoader;
-import bhutan.eledger.common.excel.XLSXLoader;
+import am.iunetworks.lib.common.persistence.search.SearchResult;
+import bhutan.eledger.application.port.in.epayment.paymentadvice.CreatePaymentAdviceUseCase;
+import bhutan.eledger.application.port.in.epayment.paymentadvice.SearchPaymentAdviceUseCase;
+import bhutan.eledger.application.port.out.epayment.paymentadvice.PaymentAdviceRepositoryPort;
+import bhutan.eledger.common.excel.ExcelLoader;
+import bhutan.eledger.domain.epayment.PaymentAdvice;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.test.context.TestPropertySource;
+import org.xml.sax.SAXException;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.math.BigDecimal;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
@@ -25,13 +38,14 @@ class ReconciliationUploadTest {
 
     @Test
     void excelLoadTest() {
-        ReconciliationExcelLoader loader = new ReconciliationExcelLoader();
+        ExcelLoader loader = new ExcelLoader();
         try {
             Path resourceDirectory = Paths.get("src","test","resources", "files");
             String absolutePath = resourceDirectory.toFile().getAbsolutePath();
 
-            FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation.xlsx");
-            loader.load(fis, true);
+            FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation upload file.xlsx");
+            loader.load(fis, "rId1", (row, column, type, value) ->
+                    System.out.println("Row:" + row + " Col:" + column + " Value:" + value + " Type:" + type));
         } catch (Exception e) {
             e.printStackTrace();
         }
