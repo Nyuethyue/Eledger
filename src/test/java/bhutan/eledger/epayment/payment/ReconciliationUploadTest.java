c6f37ee854d6ccf7002535f2c15d745bc6789545
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-22 15:38:51 +0400

    BP-359 Bank excel statemenst upload

--- src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
+++ src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
@@ -1,30 +1,16 @@
 package bhutan.eledger.epayment.payment;
 
-import am.iunetworks.lib.common.persistence.search.SearchResult;
-import bhutan.eledger.application.port.in.epayment.paymentadvice.CreatePaymentAdviceUseCase;
-import bhutan.eledger.application.port.in.epayment.paymentadvice.SearchPaymentAdviceUseCase;
-import bhutan.eledger.application.port.out.epayment.paymentadvice.PaymentAdviceRepositoryPort;
 import bhutan.eledger.common.excel.ExcelLoader;
-import bhutan.eledger.domain.epayment.PaymentAdvice;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.test.context.TestPropertySource;
-import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.math.BigDecimal;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
