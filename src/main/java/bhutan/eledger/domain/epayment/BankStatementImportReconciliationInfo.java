4faa3026560b26dfd51939f9fa5b48c120311e56
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-22 14:51:14 +0400

    BP-359 Bank excel statemenst upload

--- src/main/java/bhutan/eledger/domain/epayment/BankStatementImportReconciliationInfo.java
+++ src/main/java/bhutan/eledger/domain/epayment/BankStatementImportReconciliationInfo.java
@@ -7,9 +7,9 @@ import java.time.LocalDate;
 
 @Data(staticConstructor = "of")
 public class BankStatementImportReconciliationInfo {
-
     private final String transactionId;
     private final String bankId;
     private final LocalDate paymentDate;
     private final BigDecimal amount;
 }
+
