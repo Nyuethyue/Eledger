a04aa662421688f9e336f685db2a4d1305926f40
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-22 14:51:14 +0400

    BP-359 Bank excel statemenst upload

--- src/main/java/bhutan/eledger/application/port/in/epayment/reconciliation/BankStatementImportUseCase.java
+++ src/main/java/bhutan/eledger/application/port/in/epayment/reconciliation/BankStatementImportUseCase.java
@@ -3,18 +3,13 @@ package bhutan.eledger.application.port.in.epayment.reconciliation;
 import bhutan.eledger.domain.epayment.BankStatementImportReconciliationInfo;
 import lombok.Data;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Map;
+import java.util.List;
 
 public interface BankStatementImportUseCase {
 
-    BankStatementImportReconciliationInfo importStatements(ImportBankStatementsCommand command);
+    List<BankStatementImportReconciliationInfo> importStatements(ImportBankStatementsCommand command);
 
     @Data
     class ImportBankStatementsCommand {
--- src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
+++ src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
@@ -1,30 +1,18 @@
 package bhutan.eledger.epayment.payment;
 
-import am.iunetworks.lib.common.persistence.search.SearchResult;
-import bhutan.eledger.application.port.in.epayment.paymentadvice.CreatePaymentAdviceUseCase;
-import bhutan.eledger.application.port.in.epayment.paymentadvice.SearchPaymentAdviceUseCase;
-import bhutan.eledger.application.port.out.epayment.paymentadvice.PaymentAdviceRepositoryPort;
-import bhutan.eledger.common.excel.ExcelLoader;
-import bhutan.eledger.domain.epayment.PaymentAdvice;
+import bhutan.eledger.common.excel.XLSLoader;
+import bhutan.eledger.common.excel.XLSXLoader;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.test.context.TestPropertySource;
-import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.math.BigDecimal;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
@@ -37,18 +25,22 @@ class ReconciliationUploadTest {
     }
 
     @Test
-    void excelLoadTest() {
-        ExcelLoader loader = new ExcelLoader();
-        try {
+    void excelLoadTest() throws FileNotFoundException, IOException {
+
             Path resourceDirectory = Paths.get("src","test","resources", "files");
             String absolutePath = resourceDirectory.toFile().getAbsolutePath();
 
-            FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation upload file.xlsx");
-            loader.load(fis, "rId1", (row, column, type, value) ->
+            XLSXLoader xlsxLoader = new XLSXLoader();
+            try (FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation.xlsx")) {
+                xlsxLoader.load(fis, "rId1", (sheetId, row, column, type, value) ->
+                        System.out.println("Row:" + row + " Col:" + column + " Value:" + value + " Type:" + type));
+            }
+
+            XLSLoader xlsLoader = new XLSLoader();
+            try (FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation.xls")) {
+                xlsLoader.load(fis, "rId1", (sheetId, row, column, type, value) ->
                     System.out.println("Row:" + row + " Col:" + column + " Value:" + value + " Type:" + type));
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        Assertions.assertEquals(1, 1);
+
     }
 }
