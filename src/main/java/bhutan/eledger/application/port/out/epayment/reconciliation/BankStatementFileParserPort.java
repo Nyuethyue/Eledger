51370248d0033126a1bd87ba7a2034dd00f28f12
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-22 14:51:14 +0400

    BP-359 Bank excel statemenst upload

--- src/main/java/bhutan/eledger/application/port/out/epayment/reconciliation/BankStatementFileParserPort.java
+++ src/main/java/bhutan/eledger/application/port/out/epayment/reconciliation/BankStatementFileParserPort.java
@@ -1,9 +1,10 @@
 package bhutan.eledger.application.port.out.epayment.reconciliation;
 
+import bhutan.eledger.application.port.in.epayment.reconciliation.BankStatementImportUseCase;
 import bhutan.eledger.domain.epayment.BankStatementImportReconciliationInfo;
 
 import java.util.List;
 
 public interface BankStatementFileParserPort {
-    List<BankStatementImportReconciliationInfo> getStatements(String filePath);
+    List<BankStatementImportReconciliationInfo> getStatements(BankStatementImportUseCase.ImportBankStatementsCommand command);
 }
