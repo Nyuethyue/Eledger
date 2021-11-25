ebd0070eaef9ab7e5d41e7ec57e1b2ad0df5904f
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-25 20:38:32 +0400

    BP-359 Bank excel statemenst upload

--- src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
+++ src/test/java/bhutan/eledger/epayment/payment/ReconciliationUploadTest.java
@@ -1,17 +1,20 @@
 package bhutan.eledger.epayment.payment;
 
+import bhutan.eledger.application.port.in.epayment.reconciliation.BankStatementImportUseCase;
 import bhutan.eledger.common.excel.ReconciliationExcelLoader;
-import bhutan.eledger.common.excel.XLSXLoader;
+import bhutan.eledger.domain.epayment.BankStatementImportReconciliationInfo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
@@ -19,23 +22,39 @@ import java.nio.file.Paths;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReconciliationUploadTest {
 
+    @Autowired
+    private BankStatementImportUseCase bankStatementImportUseCase;
+
     @AfterEach
     void afterEach() {
     }
 
     @Test
-    void excelLoadTest() {
-        // http://10.11.1.13/resources/file/reports/ee9c1eeb-cef2-472d-b6d0-ddeb015a370b/profiles.xlsx
+    void excelLoadFromFileTest() {
         ReconciliationExcelLoader loader = new ReconciliationExcelLoader();
         try {
             Path resourceDirectory = Paths.get("src","test","resources", "files");
             String absolutePath = resourceDirectory.toFile().getAbsolutePath();
 
-            FileInputStream fis = new FileInputStream( absolutePath + "\\" + "Reconciliation.xlsx");
-            loader.load(fis, true);
+            List<BankStatementImportReconciliationInfo> resOld = loader.load(new FileInputStream( absolutePath + "\\" + "Reconciliation.xls"), false);
+            Assertions.assertTrue(resOld.size() > 0, "Empty result for old!");
+            List<BankStatementImportReconciliationInfo> resNew = loader.load(new FileInputStream( absolutePath + "\\" + "Reconciliation.xlsx"), true);
+            Assertions.assertTrue(resNew.size() > 0, "Empty result for new!");
         } catch (Exception e) {
             e.printStackTrace();
+            Assertions.assertTrue(false);
         }
-        Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    void excelDownloadTest() {
+        String filePath = "/resources/file/reports/ee9c1eeb-cef2-472d-b6d0-ddeb015a370b/profiles.xlsx";
+        //String filePath = "/resources/file/sources.pdf";
+        BankStatementImportUseCase.ImportBankStatementsCommand command =
+                new BankStatementImportUseCase.ImportBankStatementsCommand(
+                        "TAB12345", filePath);
+
+        List<BankStatementImportReconciliationInfo> result = bankStatementImportUseCase.importStatements(command);
+        Assertions.assertTrue(result.size() > 0, "Empty result for excel file!");
     }
 }
