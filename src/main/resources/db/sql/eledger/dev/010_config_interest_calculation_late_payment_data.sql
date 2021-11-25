5cbf0655329e1cd81b47f8e3da120afbd0507067
vahagn.geghamyan <vahagn.geghamyan@iunetworks.am>
2021-11-25 23:52:49 +0400

    BP-354: Insert interest late payment config data

---
+++ src/main/resources/db/sql/eledger/dev/010_config_interest_calculation_late_payment_data.sql
@@ -0,0 +1,22 @@
+INSERT INTO eledger_config.el_interest_calculation( id
+                                                  , transaction_type_id
+                                                  , accounting_action_type_id
+                                                  , start_date_condition
+    --, days_count_condition
+                                                  , percent_condition
+    --, max_amount_condition
+                                                  , in_amount_condition
+                                                  , case_condition
+                                                  , description
+                                                  , start_of_validity)
+VALUES ( nextval('eledger_config.el_interest_calculation_id_seq')
+       , (SELECT id FROM eledger_config.el_transaction_type WHERE name = 'LIABILITY')
+       , (SELECT id FROM eledger_config.el_accounting_action_type WHERE name = 'INTEREST')
+       , '[transaction].[deadline]'
+           --, '10000'
+       , '[property].[YEARLY_PERCENT] / 100 / [function].[current_year_days_count]'
+           --, '[transaction].[amount]'
+       , '[function].[remain_amount]'
+       , '[transaction].[deadline] < p_calculation_date'
+       , 'LATE PAYMENT'
+       , '20210101');
