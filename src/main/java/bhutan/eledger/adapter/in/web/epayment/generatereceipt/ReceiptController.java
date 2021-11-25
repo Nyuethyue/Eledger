cd1540bf71d1737101f4e83c65bd160b8249e562
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-11-25 22:05:21 +0400

    BP-352: Generate receipt - provide endpoint
    Change-Id: Ic077b134e090110f6d59c9d8a2f1446e8a108c31

---
+++ src/main/java/bhutan/eledger/adapter/in/web/epayment/generatereceipt/ReceiptController.java
@@ -0,0 +1,28 @@
+package bhutan.eledger.adapter.in.web.epayment.generatereceipt;
+
+import bhutan.eledger.application.port.in.epayment.generatereceipt.GenerateCashReceiptUseCase;
+import bhutan.eledger.domain.epayment.generatereceipt.Receipt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment/receipt")
+@RequiredArgsConstructor
+public class ReceiptController {
+
+    private final GenerateCashReceiptUseCase generateCashReceiptUseCase;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody GenerateCashReceiptUseCase.GenerateCashReceiptCommand command) {
+        Receipt receipt = generateCashReceiptUseCase.generate(command);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(receipt);
+    }
+}
