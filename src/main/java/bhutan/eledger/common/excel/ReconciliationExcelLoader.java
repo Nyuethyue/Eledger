f9a20dbfa6c42700f587e319f8af9815dd10c23a
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-11-25 18:23:31 +0400

    BP-359 Bank excel statemenst upload

--- src/main/java/bhutan/eledger/common/excel/ReconciliationExcelLoader.java
+++ src/main/java/bhutan/eledger/common/excel/ReconciliationExcelLoader.java
@@ -1,27 +1,106 @@
 package bhutan.eledger.common.excel;
 
+import am.iunetworks.lib.common.validation.ViolationException;
 import bhutan.eledger.domain.epayment.BankStatementImportReconciliationInfo;
+import lombok.extern.log4j.Log4j2;
+import org.apache.poi.hssf.record.CellRecord;
+import org.apache.poi.hssf.record.NumberRecord;
+import org.apache.poi.hssf.record.Record;
+import org.apache.poi.ss.usermodel.DateUtil;
 
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
-
+@Log4j2
 public class ReconciliationExcelLoader  implements ExcelCellReceiver {
-    private List<BankStatementImportReconciliationInfo> result = new LinkedList<>();
+    private int currentRowIndex;
+    private BankStatementImportReconciliationInfo currentRowValue;
 
-    public List<BankStatementImportReconciliationInfo> load(InputStream inputStream, boolean isXLSX) {
-        if (isXLSX) {
-            XLSLoader xlsLoader = new XLSLoader();
-            xlsLoader.load(inputStream, "rId1", this);
-        } else {
-            XLSXLoader xlsxLoader = new XLSXLoader();
-            xlsxLoader.load(inputStream, "rId1", this);
+    private List<BankStatementImportReconciliationInfo> result;
+
+
+    public List<BankStatementImportReconciliationInfo> load(InputStream inputStream, boolean isXLSX) throws Exception {
+        try {
+            if (isXLSX) {
+                XLSXLoader xlsxLoader = new XLSXLoader();
+                xlsxLoader.load(inputStream, "rId1", this);
+            } else {
+                XLSLoader xlsLoader = new XLSLoader();
+                xlsLoader.load(inputStream, "rId1", this);
+            }
+        } catch (Exception e) {
+            log.error("Failed to parse excel file");
+            throw new Exception("Error loading excel file", e);
         }
         return result;
     }
 
     @Override
     public void newCell(String sheetId, int row, int column, String type, String value) {
-        System.out.println("Row:" + row + " Col:" + column + " Value:" + value + " Type:" + type);
+        if (row > 0) { // 0 is table header
+            if (currentRowIndex != row) {
+                if (null != currentRowValue) {
+                    result.add(currentRowValue);
+                }
+                currentRowValue = new BankStatementImportReconciliationInfo();
+            }
+
+            if (0 == column) {
+                currentRowValue.setTransactionId(value);
+            } else if (1 == column) {
+                currentRowValue.setBankId(value);
+            } else if (2 == column) {
+                currentRowValue.setRefNo(value);
+            } else if (3 == column) {
+                double excelDate = Double.parseDouble(value);
+                currentRowValue.setPaymentDate(DateUtil.getLocalDateTime(excelDate).toLocalDate());
+            } else if (4 == column) {
+                currentRowValue.setAmount(new BigDecimal(value));
+            }
+            currentRowIndex = row;
+        }
+    }
+
+    @Override
+    public void startDocument() {
+        currentRowIndex = -1;
+        currentRowValue = null;
+        result = new LinkedList<>();
+    }
+
+    @Override
+    public void newCell(String sheetId, CellRecord record) {
+        int row = record.getRow();
+        int column = record.getColumn();
+        if (row > 0) { // 0 is table header
+            if (currentRowIndex != row) {
+                if (null != currentRowValue) {
+                    result.add(currentRowValue);
+                }
+                currentRowValue = new BankStatementImportReconciliationInfo();
+            }
+
+            if (0 == column) {
+                currentRowValue.setTransactionId(Double.toString(((NumberRecord)record).getValue()));
+            } else if (1 == column) {
+                currentRowValue.setBankId(Double.toString(((NumberRecord)record).getValue()));
+            } else if (2 == column) {
+                double value = ((NumberRecord)record).getValue();
+                currentRowValue.setRefNo(Double.toString(value));
+            } else if (3 == column) {
+                double excelDate = ((NumberRecord)record).getValue();
+                currentRowValue.setPaymentDate(DateUtil.getLocalDateTime(excelDate).toLocalDate());
+            } else if (4 == column) {
+                currentRowValue.setAmount(new BigDecimal(((NumberRecord)record).getValue()));
+            }
+            currentRowIndex = row;
+        }
+    }
+
+    @Override
+    public void endDocument() {
+        result.add(currentRowValue);
     }
 }
