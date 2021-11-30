d876bf69fac53337f0e13a7ed34d280d5f42461f
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-11-30 21:22:53 +0400

    BP-364: Push eledger to complete liability
    Change-Id: I56ac0be1c62611b03644f4defb33a747fa969768

--- src/main/java/bhutan/eledger/adapter/out/persistence/epayment/payment/EledgerPaymentTransactionAdapter.java
+++ src/main/java/bhutan/eledger/adapter/out/persistence/epayment/payment/EledgerPaymentTransactionAdapter.java
@@ -1,18 +1,53 @@
 package bhutan.eledger.adapter.out.persistence.epayment.payment;
 
+import bhutan.eledger.application.port.in.eledger.transaction.CreateTransactionsUseCase;
 import bhutan.eledger.application.port.out.epayment.payment.EledgerPaymentTransactionPort;
 import bhutan.eledger.domain.epayment.payment.Receipt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Log4j2
 @Component
 @RequiredArgsConstructor
 class EledgerPaymentTransactionAdapter implements EledgerPaymentTransactionPort {
+    private final CreateTransactionsUseCase createTransactionsUseCase;
 
     @Override
     public void create(Receipt receipt) {
-        log.warn("Not implemented!!!");
+
+        var transactionsCreationCommand = new CreateTransactionsUseCase.CreateTransactionsCommand(
+                receipt.getReceiptNumber(),
+                new CreateTransactionsUseCase.TaxpayerCommand(
+                        receipt.getTaxpayer().getTpn(),
+                        receipt.getTaxpayer().getName()
+                ),
+                receipt.getPayments()
+                        .stream()
+                        .map(p ->
+                                new CreateTransactionsUseCase.TransactionCommand(
+                                        p.getGlAccount().getCode(),
+                                        receipt.getCreationDateTime().toLocalDate(),
+                                        p.getPaidAmount(),
+                                        "PAYMENT",
+                                        Set.of(
+                                                new CreateTransactionsUseCase.TransactionAttributeCommand(
+                                                        "TARGET_DRN",
+                                                        receipt.getDrn()
+                                                ),
+                                                new CreateTransactionsUseCase.TransactionAttributeCommand(
+                                                        "TARGET_TRANSACTION_ID",
+                                                        p.getElTargetTransactionId().toString()
+                                                )
+                                        )
+                                )
+                        )
+                        .collect(Collectors.toUnmodifiableSet())
+        );
+
+        createTransactionsUseCase.create(transactionsCreationCommand);
     }
 }
