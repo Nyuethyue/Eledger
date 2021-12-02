46adc060afd5c20330849298653392b326c1a8ab
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-12-02 19:50:49 +0400

    ADD_RECEIPT_GET_MAPPING

---
+++ src/main/java/bhutan/eledger/domain/epayment/deposit/DepositStatus.java
@@ -0,0 +1,26 @@
+package bhutan.eledger.domain.epayment.deposit;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@RequiredArgsConstructor
+public enum DepositStatus {
+    PENDING_RECONCILIATION("PENDING_RECONCILIATION"),
+    PRE_RECONCILED("PRE_RECONCILED"),
+    RECONCILED("RECONCILED"),
+    CANCELLED("CANCELLED"),
+    BOUNCED("BOUNCED");
+
+    private final String value;
+
+    public static DepositStatus of(@NonNull String value) {
+        return Arrays.stream(values())
+                .filter(element -> element.value.equals(value))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Illegal value: [" + value + "]. Possible values: " + Arrays.toString(values())));
+    }
+}
---
+++ src/main/resources/db/sql/epayment/007_deposit_schema.sql
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS epayment.deposit
+(
+    id                     bigint    NOT NULL,
+    payment_mode           bigint   NOT NULL,
+    bank_deposit_date      date NOT NULL,
+    last_printed_date      timestamp NOT NULL,
+    amount                 numeric(20, 2) NOT NULL,
+    status                 varchar   NOT NULL,
+    creation_date_time     timestamp NOT NULL,
+    taxpayer_id            bigint    NOT NULL
+);
+
+ALTER TABLE epayment.deposit
+    ADD CONSTRAINT pk_deposit PRIMARY KEY (id);
+
+ALTER TABLE ONLY epayment.deposit
+    ADD CONSTRAINT fk_deposit_payment_mode
+        FOREIGN KEY (payment_mode) REFERENCES ref.payment_mode (id);
+
+CREATE SEQUENCE epayment.deposit_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    START 1
+    CACHE 1
+    NO CYCLE
+    OWNED BY epayment.deposit.id;
+-----------------------------------------------------------------------------------------------------------------------------
