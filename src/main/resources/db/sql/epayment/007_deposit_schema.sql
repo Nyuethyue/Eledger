004f8c9f78d22af35b3264df149b9bcaf926c363
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-12-02 19:50:49 +0400

    ADD_RECEIPT_GET_MAPPING

--- src/main/resources/db/sql/epayment/007_deposit_schema.sql
+++ src/main/resources/db/sql/epayment/007_deposit_schema.sql
@@ -1,13 +1,14 @@
 -----------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS epayment.deposit
 (
-    id                     bigint   NOT NULL,
-    payment_mode_id        bigint   NOT NULL,
-    bank_deposit_date      date     NOT NULL,
+    id                     bigint    NOT NULL,
+    payment_mode           bigint   NOT NULL,
+    bank_deposit_date      date NOT NULL,
     last_printed_date      timestamp NOT NULL,
     amount                 numeric(20, 2) NOT NULL,
     status                 varchar   NOT NULL,
     creation_date_time     timestamp NOT NULL,
+    taxpayer_id            bigint    NOT NULL
 );
 
 ALTER TABLE epayment.deposit
@@ -15,8 +16,7 @@ ALTER TABLE epayment.deposit
 
 ALTER TABLE ONLY epayment.deposit
     ADD CONSTRAINT fk_deposit_payment_mode
-        FOREIGN KEY (payment_mode_id)
-            REFERENCES ref.payment_mode (id);
+        FOREIGN KEY (payment_mode) REFERENCES ref.payment_mode (id);
 
 CREATE SEQUENCE epayment.deposit_id_seq
     INCREMENT BY 1
@@ -25,60 +25,4 @@ CREATE SEQUENCE epayment.deposit_id_seq
     CACHE 1
     NO CYCLE
     OWNED BY epayment.deposit.id;
------------------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS epayment.deposit_receipt
-(
-    id                     bigint    NOT NULL,
-    deposit_id             bigint    NOT NULL,
-    receipt_id             bigint    NOT NULL,
-    receipt_number         varchar NOT NULL
-);
-
-ALTER TABLE epayment.deposit_receipt
-    ADD CONSTRAINT pk_deposit_receipt PRIMARY KEY (id);
-
-ALTER TABLE ONLY epayment.deposit_receipt
-    ADD CONSTRAINT fk_deposit_deposit_id
-        FOREIGN KEY (deposit_id) REFERENCES epayment.deposit (id);
-
-ALTER TABLE ONLY epayment.deposit_receipt
-    ADD CONSTRAINT fk_deposit_receipt_id
-        FOREIGN KEY (receipt_id) REFERENCES epayment.ep_receipt (id);
-
-
-CREATE SEQUENCE epayment.deposit_receipt_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    START 1
-    CACHE 1
-    NO CYCLE
-    OWNED BY epayment.deposit_receipt.id;
------------------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS epayment.deposit_denomination_counts
-(
-    id                     bigint    NOT NULL,
-    deposit_id             bigint    NOT NULL,
-    denomination_id        bigint    NOT NULL,
-    "count"                bigint    NOT NULL
-);
-
-ALTER TABLE epayment.deposit_denomination_counts
-    ADD CONSTRAINT pk_deposit_denomination_counts PRIMARY KEY (id);
-
-ALTER TABLE ONLY epayment.deposit_denomination_counts
-    ADD CONSTRAINT fk_deposit_denomination_counts_deposit_id
-        FOREIGN KEY (deposit_id) REFERENCES epayment.deposit (id);
-
-ALTER TABLE ONLY epayment.deposit_denomination_counts
-    ADD CONSTRAINT fk_deposit_denomination_counts_denomination_id
-        FOREIGN KEY (denomination_id) REFERENCES ref.denomination (id);
-
-
-CREATE SEQUENCE epayment.deposit_denomination_counts_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    START 1
-    CACHE 1
-    NO CYCLE
-    OWNED BY epayment.deposit_denomination_counts.id;
 -----------------------------------------------------------------------------------------------------------------------------
