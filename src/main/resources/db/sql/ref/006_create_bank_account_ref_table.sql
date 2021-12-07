58d3ca4ff9d936b67a607c6587dff391891c62e3
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-12-08 01:00:41 +0400

    BP-507: BE - GL code to bank account mapping - FIX db creation
    Change-Id: I5b2776f6a68bef4322080e180adf06ebb19ddfdb

--- src/main/resources/db/sql/ref/006_create_bank_account_ref_table.sql
+++ src/main/resources/db/sql/ref/006_create_bank_account_ref_table.sql
@@ -1,4 +1,23 @@
 ------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE ref.bank_account_gl_account_part
+(
+    id   bigint  NOT NULL,
+    code varchar NOT NULL
+);
+
+ALTER TABLE ref.bank_account_gl_account_part
+    ADD CONSTRAINT pk_bank_account_gl_account_part PRIMARY KEY (id);
+
+CREATE SEQUENCE ref.bank_account_gl_account_part_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    START 1
+    CACHE 1
+    NO CYCLE
+    OWNED BY ref.bank_account_gl_account_part.id;
+
+------------------------------------------------------------------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS ref.bank_account
 (
     id                              bigint  NOT NULL,
