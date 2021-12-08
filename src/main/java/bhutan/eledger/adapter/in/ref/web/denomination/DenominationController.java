d4abdf6f9c6a29f44fc0ba56cb144938069928dc
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-12-08 15:15:48 +0400

    BP-357_DEPOSIT_DENOMINATION_QUERIES

--- src/main/java/bhutan/eledger/adapter/in/ref/web/denomination/DenominationController.java
+++ src/main/java/bhutan/eledger/adapter/in/ref/web/denomination/DenominationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/ref/denomination/mode")
+@RequestMapping("/ref/denomination")
 @RequiredArgsConstructor
 class DenominationController {
 
