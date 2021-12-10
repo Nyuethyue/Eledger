c45bd88687a3bf60f88a9dd0f4df837bad26f456
Passang lhamo <passang22lhamo@gmail.com>
2021-12-10 20:04:37 +0600

    BP-593:BE - GL code to bank account mapping

--- src/main/java/bhutan/eledger/adapter/in/ref/web/bankaccount/RefBankAccountController.java
+++ src/main/java/bhutan/eledger/adapter/in/ref/web/bankaccount/RefBankAccountController.java
@@ -2,7 +2,6 @@ package bhutan.eledger.adapter.in.ref.web.bankaccount;
 
 import bhutan.eledger.application.port.in.ref.bankaccount.CreateRefBankAccountUseCase;
 import bhutan.eledger.application.port.in.ref.bankaccount.ReadRefBankAccountUseCase;
-import bhutan.eledger.application.port.in.ref.bankaccount.UpdateRefBankAccountUseCase;
 import bhutan.eledger.domain.ref.bankaccount.RefBankAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,7 +19,6 @@ class RefBankAccountController {
 
     private final CreateRefBankAccountUseCase createRefBankAccountUseCase;
     private final ReadRefBankAccountUseCase readRefBankAccountUseCase;
-    private final UpdateRefBankAccountUseCase updateRefBankAccountUseCase;
 
     @PostMapping
     public ResponseEntity<Object> create(@RequestBody CreateRefBankAccountUseCase.CreateBankAccountCommand command) {
@@ -59,14 +57,6 @@ class RefBankAccountController {
     @ResponseStatus(value = HttpStatus.OK)
     public RefBankAccount readPrimaryAccByGlCode(@PathVariable String glPartFullCode) {
         return readRefBankAccountUseCase.readPrimaryAccByGlPartFullCode(glPartFullCode);
-    }
-
-    @PutMapping(value = "/updatePrimaryBankAccount/{id}")
-    public ResponseEntity<Object> updatePrimaryBankAccount(@PathVariable Long id) {
-        updateRefBankAccountUseCase.updatePrimaryBankAccount(id);
-        return ResponseEntity
-                .noContent()
-                .build();
     }
 
     @GetMapping(value = "/allByBankIdAndGlPartCode", produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/bhutan/eledger/application/port/in/ref/bankaccount/UpdateRefBankAccountUseCase.java
+++
@@ -1,9 +0,0 @@
-package bhutan.eledger.application.port.in.ref.bankaccount;
-
-import com.sun.istack.NotNull;
-import org.springframework.validation.annotation.Validated;
-
-@Validated
-public interface UpdateRefBankAccountUseCase {
-    void updatePrimaryBankAccount(@NotNull Long id);
-}
