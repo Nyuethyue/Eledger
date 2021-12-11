afd790bc93ce129b13e70846295567f87cd7c094
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2021-12-11 18:29:04 +0400

    BP-324_RECONCILIATION

--- src/main/java/bhutan/eledger/application/port/in/epayment/deposit/ApproveReconciliationUseCase.java
+++ src/main/java/bhutan/eledger/application/port/in/epayment/deposit/ApproveReconciliationUseCase.java
@@ -1,6 +1,7 @@
 package bhutan.eledger.application.port.in.epayment.deposit;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
@@ -10,22 +11,21 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
+import java.util.List;
 
 @Validated
 public interface ApproveReconciliationUseCase {
 
     void approveDepositReconciliation(@Valid ApproveReconciliationUseCase.ApproveDepositReconciliationCommand command);
 
-    @Getter
-    @ToString
+    @Data
     class ApproveDepositReconciliationCommand {
-        @Valid
         @NotNull
         @NotEmpty
-        private final Collection<String> depositNumbers;
+        private final List<String> depositNumbers;
 
         @JsonCreator
-        public ApproveDepositReconciliationCommand(Collection<String> depositNumbers) {
+        public ApproveDepositReconciliationCommand(@JsonProperty("depositNumbers") List<String> depositNumbers) {
             this.depositNumbers = depositNumbers;
         }
     }
