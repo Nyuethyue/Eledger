986b2e73d65eb0fe842ba02d975b64926f1d2c9d
Beno Arakelyan <beno.arakelyan@gmail.com>
2021-12-23 15:30:59 +0400

    Remove unused sql file
    Change-Id: I951f86076ea6ee53e0082672853d5a7964a5785a

--- src/main/resources/db/sql/eledger/dev/901_delete_redundant_gl_accounts.sql
+++
@@ -1,3 +0,0 @@
---  delete redundant gl_account_part
-delete from eledger_config.el_gl_account_part_description where gl_account_part_id in (17, 18);
-delete from eledger_config.el_gl_account_part where id in (17, 18);
