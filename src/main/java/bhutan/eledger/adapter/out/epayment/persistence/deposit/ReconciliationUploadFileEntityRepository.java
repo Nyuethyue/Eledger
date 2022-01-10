614599114c74db0f9c32e46498792305d3a0044d
Aleksandr Andriasyan <aleksandr.andriasyan@iunetworks.am>
2022-01-10 17:22:47 +0400

    BP-776_RECONCILIATION_HISTORY

--- src/main/java/bhutan/eledger/adapter/out/epayment/persistence/deposit/ReconciliationUploadFileEntityRepository.java
+++ src/main/java/bhutan/eledger/adapter/out/epayment/persistence/deposit/ReconciliationUploadFileEntityRepository.java
@@ -2,5 +2,5 @@ package bhutan.eledger.adapter.out.epayment.persistence.deposit;
 
 import am.iunetworks.lib.common.persistence.spring.querydsl.CustomQuerydslJpaRepository;
 
-public interface ReconciliationUploadFileEntityRepository extends CustomQuerydslJpaRepository<ReconciliationUploadFileEntity, Long> {
+interface ReconciliationUploadFileEntityRepository extends CustomQuerydslJpaRepository<ReconciliationUploadFileEntity, Long> {
 }
