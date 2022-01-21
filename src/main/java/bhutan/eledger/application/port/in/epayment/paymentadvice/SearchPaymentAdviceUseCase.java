9ad9dcf9734b9fe183050f4614f26de5b215e460
Passang lhamo <passang22lhamo@gmail.com>
2022-01-21 18:46:24 +0600

    BP-996:Add use case and service file

--- src/main/java/bhutan/eledger/application/port/in/epayment/paymentadvice/SearchPaymentAdviceUseCase.java
+++ src/main/java/bhutan/eledger/application/port/in/epayment/paymentadvice/SearchPaymentAdviceUseCase.java
@@ -1,19 +1,13 @@
 package bhutan.eledger.application.port.in.epayment.paymentadvice;
 
 import am.iunetworks.lib.common.persistence.search.SearchResult;
-import bhutan.eledger.domain.epayment.paymentadvice.FlatPaymentAdvice;
 import bhutan.eledger.domain.epayment.paymentadvice.PaymentAdvice;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
-import java.util.Collection;
 
 @Validated
 public interface SearchPaymentAdviceUseCase {
@@ -31,25 +25,5 @@ public interface SearchPaymentAdviceUseCase {
         private final String tpn;
         private final String pan;
     }
-
-    Collection<FlatPaymentAdvice> searchByDrns(@Valid SearchPaymentAdviceUseCase.SearchPaymentAdviceByDrnCommand command);
-
-    @Data
-    @NoArgsConstructor
-    class SearchPaymentAdviceByDrnCommand {
-        private Collection<DrnCommand> drnCommands;
-    }
-
-    @Data
-    class DrnCommand {
-        @NotNull
-        private final String drn;
-
-        @JsonCreator
-        public DrnCommand(String drn) {
-            this.drn = drn;
-        }
-    }
-
 
 }
