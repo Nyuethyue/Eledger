c8a8f2e2b43fe4f8e91445f8ecccc49bc5a8ab4e
Vahagn Geghamyan <vahagn.geghamyan@iunetworks.am>
2022-02-04 12:00:16 +0400

    BP-1007: Net off functionality

--- src/main/resources/db/sql/eledger/dev/007_transaction_couples_data.sql
+++ src/main/resources/db/sql/eledger/dev/007_transaction_couples_data.sql
@@ -183,20 +183,23 @@ WHERE p.account_type = 'P'
 
 INSERT INTO eledger_config.el_transaction_couples(id, accounting_action_type_id, from_transaction_type_account_id,
                                                   to_transaction_type_account_id)
-SELECT nextval('eledger_config.el_transaction_couples_id_seq')                                     id
+select nextval('eledger_config.el_transaction_couples_id_seq')                                     id
      , (SELECT id FROM eledger_config.el_accounting_action_type WHERE name = 'REPAY_NET_NEGATIVE') accounting_action_id
-     , p.id
-     , a.id
-FROM eledger_config.el_transaction_type_gl_account p
-         INNER JOIN eledger_config.el_transaction_type pett
-                    ON pett.id = p.transaction_type_id
-         INNER JOIN eledger_config.el_transaction_type_gl_account a
-                    ON a.gl_account_id = p.gl_account_id
-         INNER JOIN eledger_config.el_transaction_type aett
-                    ON aett.id = a.transaction_type_id
-WHERE p.account_type = 'P'
-  AND a.account_type = 'A'
-  AND pett.code = 'NET_NEGATIVE'
-  AND aett.code = 'LIABILITY';
+     , f.id
+     , t.id
+from (
+         select ettga.*
+         from eledger_config.el_transaction_type_gl_account ettga
+                  inner join eledger_config.el_transaction_type ett
+                             on ettga.transaction_type_id = ett.id
+         where ett.code = 'NET_NEGATIVE'
+     ) f
+         cross join (
+    select ettga.*
+    from eledger_config.el_transaction_type_gl_account ettga
+             inner join eledger_config.el_transaction_type ett
+                        on ettga.transaction_type_id = ett.id
+    where ett.code = 'LIABILITY'
+) t;
 
 ------------------------------------------------------------------------------------------------------------------------
