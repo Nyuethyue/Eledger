e9497de9812edea62dcf2f4b82fce0c7dafc28d0
Aram Nigiyan <aram.nigiyan@iunetworks.am>
2022-03-02 17:29:14 +0400

    BP-1407: on failure case orderNo comes as URL param (non-tested commit)

--- src/main/java/bhutan/eledger/adapter/in/epayment/api/RmaTransactionRestController.java
+++ src/main/java/bhutan/eledger/adapter/in/epayment/api/RmaTransactionRestController.java
@@ -8,10 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Log4j2
@@ -43,8 +42,11 @@ class RmaTransactionRestController {
         ).build();
     }
 
+    /**
+     * @param bfs_orderNo on failure case some params are sent as URL param
+     */
     @PostMapping(path = "/failure", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public ResponseEntity<Void> failure(RmaTransactionFailUseCase.RmaTransactionFailCommand command, @RequestHeader("Back-To") String backTo) {
+    public ResponseEntity<Void> failure(RmaTransactionFailUseCase.RmaTransactionFailCommand command, @Nullable @RequestParam String bfs_orderNo, @RequestHeader("Back-To") String backTo) {
 
         log.debug("Failure request received with mapped dto: {}", command);
 
@@ -54,7 +56,7 @@ class RmaTransactionRestController {
 
         return ResponseEntity.status(HttpStatus.FOUND).location(
                 UriComponentsBuilder.fromUriString(backTo)
-                        .queryParam("orderNo", command.getOrderNo())
+                        .queryParam("orderNo", StringUtils.hasLength(bfs_orderNo) ? bfs_orderNo : command.getOrderNo())
                         .queryParam("status", "Failure")
                         .build().toUri()
         ).build();
